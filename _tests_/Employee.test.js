b074a227e9d5c831e0cc0d7c6c08ac7a1e79607c
Zdeal <jedimastershake@gmail.com>
2021-07-06 13:11:58 -0400

    adding to test files, index added

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require('../lib/Employee');
 
-test('Can get Employee instance', () => {
+test('Can set Employee instance', () => {
     const newEmployee = new Employee();
     expect(newEmployee instanceof Employee).toEqual(true);
 });
@@ -15,4 +15,34 @@ test('Can set id from constructor', () => {
     const newId = 1026;
     const newEmployee = new Employee("name", newId);
     expect(newEmployee.id).toEqual(newId);
-})
+});
+
+test('Can set email from constructor', () => {
+    const newEmail = "person@site.com";
+    const newEmployee = new Employee("name", 1026, newEmail);
+    expect(newEmployee.email).toEqual(newEmail);
+});
+
+test('Can get name via getName()', ()=> {
+    const name = "Zach";
+    const newEmployee = new Employee(name);
+    expect(newEmployee,getName()).toEqual(name);
+});
+
+test("Can get id from getId()", ()=> {
+    const newId = 1026;
+    const newEmployee = new Employee("name", newId);
+    expect(newEmployee.getId()).toEqual(newId);
+});
+
+test("Can get email from getEmail()", ()=>{
+    const newEmail = "person@site.com";
+    const newEmployee = new Employee("name", 1026, newEmail);
+    expect(newEmployee.getEmail()).toEqual(newEmail);
+});
+
+test("getRole() should return employee", ()=> {
+    const getEmployee = "Employee";
+    const newEmployee = new Employee("name", 1026, "person@site.com");
+    expect(newEmployee.getRole()).toEqual(getEmployee);
+});
