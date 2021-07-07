ea85d12caad1c363425351b0f4e678d1b3dc669f
Zdeal <jedimastershake@gmail.com>
2021-07-07 19:34:37 -0400

    good start on html render

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "index.html");
+const OUTPUT_DIR = path.resolve(__dirname, "dist")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+const render = require("./lib/htmlRenderer");
 
 const teamMembers = [];
 const idArray = [];
