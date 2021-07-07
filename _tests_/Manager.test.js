c4d8ca7f0639d0d272f900772c9f7880d649093f
Zdeal <jedimastershake@gmail.com>
2021-07-06 22:44:21 -0400

    test pass!

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,20 @@
+const { test, expect } = require('@jest/globals');
 const Manager = require('../lib/Manager');
+
+test("Can set office number from constructor", ()=>{
+    const officeNumber = "badge";
+    const employee = new Manager ("name", 1026, "person@site.com", officeNumber);
+    expect(employee.officeNumber).toEqual(officeNumber);
+});
+
+test("getRole() should return Manager", ()=>{
+    const role = "Manager";
+    const employee = new Manager ("name", 1026, "person@site.com", "badge");
+    expect(employee.getRole()).toEqual(role);
+});
+
+test("Can get office number repo from getOfficeNumber", ()=>{
+    const officeNumber = "badge";
+    const employee = new Manager ("name", 1026, "person@site.com", officeNumber);
+    expect(employee.getOfficeNumber()).toEqual(officeNumber);
+});
